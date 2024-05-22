c151a01c5dd439c3a5ddeb81d66cc160a0b0692d
Oğuzhan Çelik <oguz.bugra29@hotmail.com>
2024-05-22 20:12:08 +0300

    A Text added into the Column inside the LoginPage.kt

--- app/src/main/java/com/example/pocketcalendarv3/LoginPage.kt
+++ app/src/main/java/com/example/pocketcalendarv3/LoginPage.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -13,8 +14,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.pocketcalendarv3.ui.theme.DefaultBlue
+import com.example.pocketcalendarv3.ui.theme.fontFamily
 
 @Composable
 fun LoginPageView(modifier: Modifier = Modifier, navController: NavController) {
@@ -36,5 +42,15 @@ fun LoginPageView(modifier: Modifier = Modifier, navController: NavController) {
 
 
     ) {
+        Text(
+            text = "POCKET-CALANDER",
+            style = TextStyle(
+                color = DefaultBlue,
+                fontSize = 30.sp,
+                textAlign = TextAlign.Center,
+                fontFamily = fontFamily
+
+            )
+        )
     }
 }
