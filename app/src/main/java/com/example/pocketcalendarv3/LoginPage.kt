32af0d6c9579d8f21e53714ed9165447ebcfa74b
Oğuzhan Çelik <oguz.bugra29@hotmail.com>
2024-05-22 20:04:50 +0300

    A Column added to LoginPage.kt

--- app/src/main/java/com/example/pocketcalendarv3/LoginPage.kt
+++ app/src/main/java/com/example/pocketcalendarv3/LoginPage.kt
@@ -1,11 +1,19 @@
 package com.example.pocketcalendarv3
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 @Composable
@@ -16,4 +24,17 @@ fun LoginPageView(modifier: Modifier = Modifier, navController: NavController) {
     }
     var password by remember {
         mutableStateOf("")
-    }}
+    }
+    Column(
+        Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+
+            .padding(PaddingValues(top = 144.dp)),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+
+
+    ) {
+    }
+}
