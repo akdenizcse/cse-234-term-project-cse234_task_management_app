c429a39e8407e0207c0d69f6fed4efcb2cb4b8ef
Oğuzhan Çelik <oguz.bugra29@hotmail.com>
2024-05-22 19:39:43 +0300

    Type.kt updated

--- app/src/main/java/com/example/pocketcalendarv3/ui/theme/Type.kt
+++ app/src/main/java/com/example/pocketcalendarv3/ui/theme/Type.kt
@@ -4,8 +4,22 @@ import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.googlefonts.Font
+import androidx.compose.ui.text.googlefonts.GoogleFont
 import androidx.compose.ui.unit.sp
+import com.example.pocketcalendarv3.R
 
+val provider = GoogleFont.Provider(
+    providerAuthority = "com.google.android.gms.fonts",
+    providerPackage = "com.google.android.gms",
+    certificates = R.array.com_google_android_gms_fonts_certs
+)
+
+val fontName = GoogleFont("Righteous")
+
+val fontFamily = FontFamily(
+    Font(googleFont = fontName, fontProvider = provider)
+)
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
