4d80e132f29a3b21ff9a59d41b4537ec3e0dc94e
Oğuzhan Çelik <oguz.bugra29@hotmail.com>
2024-05-22 19:43:10 +0300

    bug fixed

--- app/src/main/java/com/example/pocketcalendarv3/ui/theme/Theme.kt
+++ app/src/main/java/com/example/pocketcalendarv3/ui/theme/Theme.kt
@@ -38,7 +38,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun PocketCalendarV2Theme(
+fun PocketCalendarV3Theme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
