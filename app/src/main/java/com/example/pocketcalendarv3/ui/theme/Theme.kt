4e2e1546b80f3deccf6aa43fc482e5db3fca09b6
Oğuzhan Çelik <oguz.bugra29@hotmail.com>
2024-05-22 19:40:22 +0300

    Theme.kt updated and font_certs.xml added to values in src.

--- app/src/main/java/com/example/pocketcalendarv3/ui/theme/Theme.kt
+++ app/src/main/java/com/example/pocketcalendarv3/ui/theme/Theme.kt
@@ -38,7 +38,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun PocketCalendarV3Theme(
+fun PocketCalendarV2Theme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
