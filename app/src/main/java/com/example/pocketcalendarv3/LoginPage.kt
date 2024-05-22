86f0159eddf26d3fb1593e2d26bd93106889e743
Oğuzhan Çelik <oguz.bugra29@hotmail.com>
2024-05-22 20:21:09 +0300

    A Spacer and Text added into Column inside the LoginPage.kt

--- app/src/main/java/com/example/pocketcalendarv3/LoginPage.kt
+++ app/src/main/java/com/example/pocketcalendarv3/LoginPage.kt
@@ -3,8 +3,10 @@ package com.example.pocketcalendarv3
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -14,7 +16,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -52,5 +57,14 @@ fun LoginPageView(modifier: Modifier = Modifier, navController: NavController) {
 
             )
         )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            text = "Task Managament App",
+            fontFamily = FontFamily.Default,
+            color = Color(0xFF9A9A9A),
+            fontWeight = FontWeight.Medium,
+            )
+
+
     }
 }
