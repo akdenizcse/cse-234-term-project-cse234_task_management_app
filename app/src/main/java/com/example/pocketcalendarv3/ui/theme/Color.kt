109825fa0931fff2553884a3e5cbae824b225cf2
Oğuzhan Çelik <oguz.bugra29@hotmail.com>
2024-05-22 19:39:09 +0300

    Color.kt updated

--- app/src/main/java/com/example/pocketcalendarv3/ui/theme/Color.kt
+++ app/src/main/java/com/example/pocketcalendarv3/ui/theme/Color.kt
@@ -1,7 +1,6 @@
 package com.example.pocketcalendarv3.ui.theme
 
 import androidx.compose.ui.graphics.Color
-
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
@@ -9,3 +8,6 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+val DefaultBlue = Color(0xFF006EE9)
+val TextFieldGray = Color(0xFF9A9A9A)
+val SoftBlue = Color(0xFFd1dff6)
