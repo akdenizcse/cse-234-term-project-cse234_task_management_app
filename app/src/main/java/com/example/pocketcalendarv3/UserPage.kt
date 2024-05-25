779fe0dc79960d58ecdbf89e3c75556a6c21e09a
Mehmet Ali Keskin <mehmetalikeskin01@gmail.com>
2024-05-25 23:47:41 +0300

    Updated the UserPage.kt.

--- app/src/main/java/com/example/pocketcalendarv3/UserPage.kt
+++ app/src/main/java/com/example/pocketcalendarv3/UserPage.kt
@@ -1,16 +1,21 @@
 package com.example.pocketcalendarv3
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Arrangement.Bottom
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Home
@@ -27,7 +32,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -35,18 +42,46 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.pocketcalendarv3.ui.theme.DefaultBlue
 import com.example.pocketcalendarv3.ui.theme.fontForDate
+import com.google.android.material.imageview.ShapeableImageView
 import androidx.compose.foundation.layout.Arrangement.End as ArrangementEnd
 
 
 @Composable
 fun UserPageView(navController: NavController) {
     NavigationBar(contentColor = Color(0xFFABCEF5)) {
-        Column(modifier = Modifier.background(DefaultBlue).fillMaxSize()
+        Column(
+            modifier = Modifier
+                .background(DefaultBlue)
+                .fillMaxSize()
 
         ) {
-            Text(text = "Profile", color = Color.White, modifier = Modifier.padding(start = 16.dp,top=40.dp),
-                fontFamily = fontForDate, fontSize = 16.sp, fontWeight = FontWeight.SemiBold
+            Text(
+                text = "Profile",
+                color = Color.White,
+                modifier = Modifier.padding(start = 16.dp, top = 40.dp),
+                fontFamily = fontForDate,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.SemiBold
             )
+            Spacer(modifier = Modifier.height(32.dp))
+            Column(
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+
+
+                Image(
+                    painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                    contentDescription = "User Profile",
+                    modifier = Modifier
+                        .size(100.dp)
+                        .clip(CircleShape)
+                        .aspectRatio(1f)
+                        .border(0.dp, Color.White, CircleShape)
+                )
+
+            }
 
             ElevatedButton(
                 onClick = { /*TODO*/ },
